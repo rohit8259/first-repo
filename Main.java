582260d7134f9a151369f5b3beca6c72f591c738
Rohit <127540114+rohit8259@users.noreply.github.com>
2023-03-12 19:59:23 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main{
+public static void main(String args[]){
+   Scanner sc=new Scanner(System.in);
+   int a,b;
+   System.out.println("Enter first number:");
+   a=sc.nextInt();
+   System.out.println("Enter second number:");
+   b=sc.nextInt();
+   int c=a+b;
+   System.out.println("Addition of two numbers is "+c);
+   }
+ }
+   
